75e93e6117522ed58e7bc602f0768561e07f85c2
Aluno IFPR <laboratorio@ifpr.edu>
2023-05-23 22:46:37 -0300

    Adicionando recursos

---
+++ src/br/ifpr/jogo/modelo/Fase.java
@@ -0,0 +1,12 @@
+package br.ifpr.jogo.modelo;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Fase {
+    private Image fundo;
+    
+    public Fase() {
+        ImageIcon carregando = new ImageIcon("recursos\\Fundo.jpg");
+    }
+}
