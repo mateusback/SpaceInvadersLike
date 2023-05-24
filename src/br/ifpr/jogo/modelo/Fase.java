32fdfab5a307851841c1a33575b218ab342d6991
Aluno IFPR <laboratorio@ifpr.edu>
2023-05-24 20:53:09 -0300

    Adicionando o personagem

--- src/br/ifpr/jogo/modelo/Fase.java
+++ src/br/ifpr/jogo/modelo/Fase.java
@@ -1,12 +1,29 @@
 package br.ifpr.jogo.modelo;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
-public class Fase {
+import br.ifpr.jogo.principal.Personagem;
+
+public class Fase extends JPanel{
     private Image fundo;
+    private Personagem personagem;
     
     public Fase() {
         ImageIcon carregando = new ImageIcon("recursos\\Fundo.jpg");
+        this.fundo = carregando.getImage();
+
+        personagem = new Personagem();
+        personagem.carregar();
+    }
+
+    public void paint(Graphics g){
+        Graphics2D graficos = (Graphics2D) g;
+        graficos.drawImage(fundo, 0,0,null);
+        graficos.drawImage(personagem.getImagem(), personagem.getPosicaoEmX(), personagem.getPosicaoEmY(), null);
+        g.dispose();
     }
 }
--- src/br/ifpr/jogo/principal/Principal.java
+++ src/br/ifpr/jogo/principal/Principal.java
@@ -2,12 +2,17 @@ package br.ifpr.jogo.principal;
 
 import javax.swing.JFrame;
 
+import br.ifpr.jogo.modelo.Fase;;
+
 public class Principal extends JFrame{
 
     public Principal() {
+        Fase fase = new Fase();
+        add(fase);
         setVisible(true);
         setTitle("Space Invaders");
-        setSize(800, 600);
+        setResizable(false);
+        setSize(1000, 1000);
         setDefaultCloseOperation(3);
         setLocationRelativeTo(null);
     }
